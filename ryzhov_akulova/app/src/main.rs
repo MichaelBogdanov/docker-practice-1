585ce849e0dc9e31c34f5d6f777059c041538e47
TheLim148 <ruzhov34@gmail.com>
2026-03-24 21:47:47 +0300

    Added structs

--- ryzhov_akulova/app/src/main.rs
+++ ryzhov_akulova/app/src/main.rs
@@ -1,3 +1,26 @@
+struct Recipe {
+    title: String,
+    ingredients: Vec<String>,
+    steps: String,
+}
+
+struct AppState {
+    recipes: Vec<Recipe>,
+}
+
+struct QueryHas {
+    has: Option<String>,
+}
+
+struct QueryInclude {
+    include: String,
+}
+
+struct Repsponse {
+    status: String,
+    message: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
