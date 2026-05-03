98a32abb5ac60a770afb54d976c59c547dba9964
TheLim148 <ruzhov34@gmail.com>
2026-05-03 14:23:54 +0300

    Docker work

--- ryzhov_akulova/app/src/main.rs
+++ ryzhov_akulova/app/src/main.rs
@@ -51,7 +51,7 @@ async fn main() {
         .route("/recipes/random", get(random_recipe))
         .with_state(shared_state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
     println!("Server running on http://localhost:3000");
